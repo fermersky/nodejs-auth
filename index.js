142f3f61f732a677aa50c0cc34a15646d1124d13
fermersky <forte201253@gmail.com>
2020-07-01 13:50:10 +0300

    fix error while jwt validation

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const { connect } = require('mongoose');
+const cors = require('cors');
 
 require('dotenv/config');
 
 const auth = require('./routes/auth');
 const privacy = require('./routes/privacy');
 const token = require('./routes/token');
+const jwtVerify = require('./jwtVerify');
 
 connect(
   process.env.DB_CONNECTION,
@@ -20,6 +22,7 @@ connect(
   }
 );
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/auth', auth);
 app.use('/privacy', privacy);
--- jwtVerify.js
+++ jwtVerify.js
@@ -2,24 +2,30 @@ const jwt = require('jsonwebtoken');
 
 async function jwtVerify(req, res, next) {
   try {
-    const authorization = req.headers.authorization;
-    if (!authorization) {
-      res.status(401).json({ err: 'authorization failed' });
+    // const authorization = req.headers.authorization;
+    // if (!authorization) {
+    //   res.status(401).json({ err: 'authorization failed' });
+    // }
+
+    // const token = authorization.split(' ')[1];
+    // if (!token) {
+    //   res.status(401).json({ err: 'authorization failed' });
+    // }
+
+    // const tokenVerify = jwt.verify(token, 'secret');
+    // if (!tokenVerify) {
+    //   res.status(401).json({ err: 'authorization failed' });
+    // }
+
+    const token = req.headers.authorization.split(' ')[1];
+
+    if (token && jwt.verify(token, 'secret')) {
+      next();
     }
 
-    const token = authorization.split(' ')[1];
-    if (!token) {
-      res.status(401).json({ err: 'authorization failed' });
-    }
-
-    const tokenVerify = jwt.verify(token, 'secret');
-    if (!tokenVerify) {
-      res.status(401).json({ err: 'authorization failed' });
-    }
-
-    next();
+    res.status(401).end({ err: 'authorization failed' });
   } catch (err) {
-    res.status(500).send(err);
+    res.status(401).send({ err: 'authorization failed' });
   }
 }
 
